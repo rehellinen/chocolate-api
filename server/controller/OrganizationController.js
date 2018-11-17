0d83a47981739c0072f4bc97b2100be33bd79265
rehellinen <912377791@qq.com>
2018-11-17 16:42:21 +0800

    注册组织API

--- server/controller/OrganizationController.js
+++ server/controller/OrganizationController.js
@@ -3,12 +3,15 @@
  *  Create By rehellinen
  *  Create On 2018/10/26 14:59
  */
-import {UserToken} from "../common/service/UserToken"
 import {SuccessMessage} from "../common/exception/SuccessMessage"
+import {OrganizationModel} from "../model/OrganizationModel"
+import {Token} from "../common/service/Token"
 
 export class OrganizationController {
   static async register (ctx) {
-    const token = await new UserToken().get(ctx.query.code)
+    let savedData = ctx.checkedParams
+    savedData.OID = Token.getSpecifiedValue(ctx)
+    const token = await new OrganizationModel().saveOne()
     throw new SuccessMessage({ data: token })
   }
 }
