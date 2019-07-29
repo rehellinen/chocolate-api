08a7c7e590f74dc2fa33bfb9edc6697600ac686a
rehellinen <rehellinen@foxmail.com>
2019-07-29 22:59:49 +0800

    better way to start the server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
- *  index.js
+ *  Server.js
  *  Create By rehellinen
  *  Create On 2019/3/4 10:29
  */
 // 启动服务器
 if (process.env.NODE_ENV === 'production') {
-  require('./dist/libs/index.js')
+  require('./dist')
 } else {
   require('@babel/register')
-  require('./src/libs/index.js')
+  require('./src')
 }
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {Server} from './libs'
+
+new Server().start()
