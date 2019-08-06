42e897bb872976908fd2e98686fbcd0fc7275055
rehellinen <rehellinen@foxmail.com>
2019-08-06 10:03:08 +0800

    update router demo

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,16 +1,12 @@
 import { controller, get, mixin, auth, validate } from '../../core'
-import { IndexController } from '../controller/IndexController'
-import { CmsMixin } from './mixins'
 
-const index = new IndexController()
-
-@mixin(CmsMixin)
 @controller('index')
 class Index {
-  @get('')
-  @validate({ name: 'index', scene: 'id' })
+  @validate({
+    name: 'index',
+    scene: 'id'
+  })
   @auth('super')
-  async getToken (ctx, next) {
-    await index.index(ctx, next)
-  }
+  @get('')
+  index = 'index.index'
 }
