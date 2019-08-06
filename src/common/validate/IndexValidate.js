5525d07413394da37c4694afa6b00b03f7a84fbb
rehellinen <rehellinen@foxmail.com>
2019-08-06 10:04:45 +0800

    Update IndexValidate.js

--- src/common/validate/IndexValidate.js
+++ src/common/validate/IndexValidate.js
@@ -3,17 +3,13 @@
  *  Create By rehellinen
  *  Create On 2018/10/12 22:38
  */
-import { Validate } from '../../core'
+import {rule, Validate} from '../../core'
 
 export class IndexValidate extends Validate {
-  constructor () {
-    super()
-    this.rules = {
-      id: ['require', 'id']
-    }
-
-    this.scene = {
-      id: ['id']
-    }
+  scene = {
+    id: ['id']
   }
+
+  @rule('require', 'id不能为空')
+  id
 }
