f34266fea859b2093adf7f3529e2f70c0d6947f1
rehellinen <rehellinen@foxmail.com>
2019-08-09 19:27:41 +0800

    multer.js - change to the new way to use config files

--- src/core/utils/multer.js
+++ src/core/utils/multer.js
@@ -5,9 +5,10 @@
  */
 import { r } from './utils'
 import Multer from 'koa-multer'
-import { config } from '../config'
+import { getConfig } from './helper'
 
-const uploadName = config.UPLOAD_NAME
+const config = getConfig()
+const uploadName = config.UPLOAD.UPLOAD_NAME
 const date = new Date()
 const month = date.getMonth().toString().length === 1
   ? '0' + (date.getMonth() + 1)
@@ -16,7 +17,7 @@ const today = `${date.getFullYear()}${month}${date.getDate()}`
 let fileName = ''
 
 const storage = Multer.diskStorage({
-  destination: r(`${config.UPLOAD_DIR}/${today}`),
+  destination: r(`${config.UPLOAD.UPLOAD_DIR}/${today}`),
   filename (ctx, file, cb) {
     const filenameArr = file.originalname.split('.')
     fileName = `${Date.now()}.${filenameArr[filenameArr.length - 1]}`
