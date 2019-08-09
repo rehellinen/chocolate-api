15c00d718c8e643ccb1d105419587e219fa6fb7c
rehellinen <rehellinen@foxmail.com>
2019-08-09 20:50:15 +0800

    update the use of config about token

--- src/core/class/Token.js
+++ src/core/class/Token.js
@@ -5,15 +5,17 @@
  */
 import md5 from 'md5'
 import cache from 'memory-cache'
-import { getRandChars } from './utils'
+import { getRandChars, getConfig } from '../utils'
 import { TokenException } from '../exception'
+
+const config = getConfig()
 
 export class Token {
   /**
    * @param scope 权限值
    * @param expireTime 过期时间
    */
-  constructor (scope, expireTime = $config.TOKEN_EXPIRES_IN) {
+  constructor (scope, expireTime = config.TOKEN.TOKEN_EXPIRES_IN) {
     this.scope = scope
     this.expireTime = expireTime
   }
@@ -23,7 +25,7 @@ export class Token {
    * @param ctx
    */
   static isSuper (ctx) {
-    Token.checkScope(ctx, $config.SCOPE.SUPER)
+    Token.checkScope(ctx, config.TOKEN.SCOPE.SUPER)
   }
 
   /**
@@ -71,7 +73,7 @@ export class Token {
   static _generateToken () {
     const str = getRandChars(32)
     const time = new Date().getTime()
-    const prefix = $config.TOKEN_PREFIX
+    const prefix = config.TOKEN.TOKEN_PREFIX
 
     return md5(`${str}-${time}-${prefix}`)
   }
--- src/core/decorator/decorator.js
+++ src/core/decorator/decorator.js
@@ -3,15 +3,18 @@
  *  Create By rehellinen
  *  Create On 2018/10/12 21:24
  */
-import { Token } from '../utils'
-import {routerMap} from './router'
+import { getConfig } from '../utils'
+import { Token } from '../class'
+import { routerMap } from './router'
+
+const config = getConfig()
 
 export const auth = (type) => {
   let scope
   if (type === 'user' || !type) {
-    scope = $config.SCOPE.USER
+    scope = config.TOKEN.SCOPE.USER
   } else if (type === 'super') {
-    scope = $config.SCOPE.SUPER
+    scope = config.TOKEN.SCOPE.SUPER
   }
   return middleware(async (ctx, next) => {
     Token.checkScope(ctx, scope)
