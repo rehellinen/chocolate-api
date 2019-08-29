71045196cad1814c8e29f9485021abad5e2a25d8
rehellinen <rehellinen@foxmail.com>
2019-08-29 17:12:16 +0800

    add more exceptions

--- src/core/exception/CoreException.js
+++ src/core/exception/CoreException.js
@@ -33,4 +33,26 @@ class TokenException extends Exception {
   }
 }
 
-export { DatabaseException, ParamsException, TokenException }
+class MethodNotAllowed extends Exception {
+  constructor (config) {
+    super(config)
+    this.setDefault({
+      httpCode: 405,
+      status: 10002,
+      message: '方法不允许'
+    })
+  }
+}
+
+class NotFound extends Exception {
+  constructor (config) {
+    super(config)
+    this.setDefault({
+      httpCode: 404,
+      status: 10001,
+      message: '该URL未定义'
+    })
+  }
+}
+
+export { DatabaseException, ParamsException, TokenException, MethodNotAllowed, NotFound }
