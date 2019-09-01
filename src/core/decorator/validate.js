566e38757751e0909eec86957c388d313237c6fb
rehellinen <rehellinen@foxmail.com>
2019-09-01 15:48:27 +0800

    handle possible errors when reading validator files

--- src/core/decorator/validate.js
+++ src/core/decorator/validate.js
@@ -1,15 +1,28 @@
-import { firstUpperCase, r, getConfig } from '../utils'
+import { firstUpperCase, r, getConfig, isClass } from '../utils'
 import { middleware } from './decorator'
+import { LibsNotFound } from '../exception'
 
 export const validateMap = new Map()
 
 export const validate = (name, scene) => {
   name = firstUpperCase(name)
-  const Validate = require(
-    r(`${getConfig('dir.validate')}/${name}`)
-  )[name]
+  const path = r(getConfig('dir.validate'), `${name}.js`)
+  let file
+  try {
+    file = require(path)
+  } catch (e) {
+    throw new LibsNotFound({
+      message: `[Validate] can't find the file\nFile Path: ${path}`
+    })
+  }
+  const Validator = file[name]
+  if (!isClass(Validator)) {
+    throw new LibsNotFound({
+      message: `[Validator] '${name}' is not a constructor\nFile Path: ${path}`
+    })
+  }
   return middleware(async (ctx, next) => {
-    await new Validate().check(ctx, scene)
+    await new Validator().check(ctx, scene)
     await next()
   })
 }
