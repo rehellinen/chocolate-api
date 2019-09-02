6f6e12d7886fb7c473b5b4ec4b7a2bb020bce6d9
rehellinen <rehellinen@foxmail.com>
2019-09-02 15:40:32 +0800

    update ValidateMethods

--- src/core/class/ValidateMethods.js
+++ src/core/class/ValidateMethods.js
@@ -1,29 +1,18 @@
 /**
- *  ValidateMethods.jsthods.js
+ *  ValidateMethods.js
  *  Create By rehellinen
  *  Create On 2018/10/13 10:24
  */
-import { ParamsException } from '../exception'
 
 export class ValidateMethods {
-  require (data = {}, field = '', errInfo = '') {
-    if (data[field] == null || data[field] === '') {
-      this.throw(errInfo || `${field}不能为空`)
-    }
+  require (data = {}, field = '') {
+    return data[field] != null && data[field] !== ''
   }
 
-  empty (data = {}, field = '', errInfo = '') {
+  empty (data = {}, field = '') {
   }
 
-  positiveInt (data = {}, field = '', errInfo = '') {
-    if (!/(^[1-9]\d*$)/.test(data[field])) {
-      this.throw(errInfo || `${field}必须为正整数`)
-    }
-  }
-
-  throw (message) {
-    throw new ParamsException({
-      message
-    })
+  positiveInt (data = {}, field = '') {
+    return /(^[1-9]\d*$)/.test(data[field])
   }
 }
